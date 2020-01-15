57d1eb78ef232c463d607d2e175c876b0d601331
Bryant <bryantandk@gmail.com>
2020-01-15 13:46:31 +1300

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { GlobalStyle } from "./style";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import { renderRoutes } from "react-router-config";
+import store from "./store/index";
 import routes from "./routes/index";
 import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
