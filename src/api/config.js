e964e4d014a575802d6007aceff851bbb08c5d19
Bryant <bryantandk@gmail.com>
2020-01-19 13:11:16 +1300

    finish api setting

---
+++ src/api/config.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const baseUrl = "http://localhost:4000";
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl
+});
+
+axiosInstance.interceptors.response.use(
+  res => res.data,
+  err => {
+    console.log(err, "network error");
+  }
+);
+
+export { axiosInstance };
