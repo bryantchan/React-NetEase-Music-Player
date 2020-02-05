49a236cf2e24a0a24909687e0b6391bb136553e5
Bryant <bryantandk@gmail.com>
2020-02-06 11:44:14 +1300

    marquee scroll for header in album page

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,13 +1,16 @@
 //src/application/Album/index.js
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import { Container, TopDesc, Menu, SongList, SongItem } from "./style";
 import { CSSTransition } from "react-transition-group";
 import Header from "../../baseUI/header";
 import Scroll from "../../baseUI/scroll";
 import { getCount, getName } from "../../api/utils";
+import style from "../../assets/global-style";
 
 function Album(props) {
   const [showStatus, setShowStatus] = useState(true);
+  const [title, setTitle] = useState("playlist");
+  const [isMarquee, setIsMarquee] = useState(false);
   const currentAlbum = {
     creator: {
       avatarUrl:
@@ -92,6 +95,27 @@ function Album(props) {
     ]
   };
 
+  const headerEl = useRef();
+  const HEADER_HEIGHT = 45;
+
+  const handleScroll = pos => {
+    let minScrollY = -HEADER_HEIGHT;
+    let percent = Math.abs(pos.y / minScrollY);
+    let headerDom = headerEl.current;
+    // 滑过顶部的高度开始变化
+    if (pos.y < minScrollY) {
+      headerDom.style.backgroundColor = style["theme-color"];
+      headerDom.style.opacity = Math.min(1, (percent - 1) / 2);
+      setTitle(currentAlbum.name);
+      setIsMarquee(true);
+    } else {
+      headerDom.style.backgroundColor = "";
+      headerDom.style.opacity = 1;
+      setTitle("playlist");
+      setIsMarquee(false);
+    }
+  };
+
   const handleBack = () => {
     setShowStatus(false);
   };
@@ -106,8 +130,13 @@ function Album(props) {
       onExited={props.history.goBack}
     >
       <Container>
-        <Header title="Back" handleClick={handleBack}></Header>
-        <Scroll bounceTop={false}>
+        <Header
+          title="Back"
+          handleClick={handleBack}
+          ref={headerEl}
+          isMarquee={isMarquee}
+        ></Header>
+        <Scroll bounceTop={false} onScroll={handleScroll}>
           <div>
             <TopDesc background={currentAlbum.coverImgUrl}>
               <div className="background">
--- src/baseUI/header/index.js
+++ src/baseUI/header/index.js
@@ -25,25 +25,33 @@ const HeaderContainer = styled.div`
 `;
 
 const Header = React.forwardRef((props, ref) => {
-  const { handleClick, title } = props;
+  const { handleClick, title, isMarquee } = props;
   return (
     <HeaderContainer ref={ref}>
       <i className="iconfont back" onClick={handleClick}>
         &#xe655;
       </i>
-      <h1>{title}</h1>
+      {isMarquee ? (
+        <marquee>
+          <h1>{title}</h1>
+        </marquee>
+      ) : (
+        <h1>{title}</h1>
+      )}
     </HeaderContainer>
   );
 });
 
 Header.defaultProps = {
   handleClick: () => {},
-  title: "title"
+  title: "title",
+  isMarquee: false
 };
 
 Header.propTypes = {
   handleClick: PropTypes.func,
-  title: PropTypes.string
+  title: PropTypes.string,
+  isMarquee: PropTypes.bool
 };
 
 export default React.memo(Header);
