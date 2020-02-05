1105c9c61c3111947663a751fa6f6a0784876e8b
Bryant <bryantandk@gmail.com>
2020-02-06 11:36:09 +1300

    fixed animation bug

--- src/application/Album/style.js
+++ src/application/Album/style.js
@@ -6,25 +6,27 @@ export const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
-  bottom: 0;
-  z-index: 1000;
-  background: ${style["background-color"]};
+  bottom: ${props => (props.play > 0 ? "60px" : 0)};
+  width: 100%;
+  z-index: 100;
+  overflow: hidden;
+  background: #f2f3f4;
   transform-origin: right bottom;
   &.fly-enter,
   &.fly-appear {
-    transform: rotateZ (30deg) translate3d (100%, 0, 0);
+    transform: rotatez(30deg) translate3d(100%, 0, 0);
   }
   &.fly-enter-active,
   &.fly-appear-active {
     transition: transform 0.3s;
-    transform: rotateZ (0deg) translate3d (0, 0, 0);
+    transform: rotatez(0deg) translate3d(0, 0, 0);
   }
   &.fly-exit {
-    transform: rotateZ (0deg) translate3d (0, 0, 0);
+    transform: rotatez(0deg) translate3d(0, 0, 0);
   }
   &.fly-exit-active {
     transition: transform 0.3s;
-    transform: rotateZ (30deg) translate3d (100%, 0, 0);
+    transform: rotatez(30deg) translate3d(100%, 0, 0);
   }
 `;
 
